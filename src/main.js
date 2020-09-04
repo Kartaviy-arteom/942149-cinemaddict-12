4b0b17926ce2bd24d94749f3adb598195c40310a
Kartaviy-arteom <kartaviy_arteom@mail.ru>
2020-09-04 19:42:58 +0300

    удаление комментариев

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Profile from "./view/profile.js";
 import MainNav from "./view/main-nav.js";
-// import SortList from "./view/sort-list.js";
 import FooterStatistics from "./view/footer-statistics-value.js";
 import {generateFilmData} from "./mock/film-data.js";
 import {generateUserStatus} from "./mock/user-status.js";
@@ -22,11 +21,10 @@ const navStats = generateNavStats(films);
 
 render(headerElement, new Profile(currentUserStatus), RenderPosition.BEFOREEND);
 render(mainElement, new MainNav(navStats), RenderPosition.AFTERBEGIN);
-// const sortListComponent = new SortList();
-// render(mainElement, sortListComponent, RenderPosition.BEFOREEND);
+
 
 const filmsListPresenter = new MovieList(mainElement);
 filmsListPresenter.init(films);
-// sortListComponent.setSortTypeChangeHandler(filmsListPresenter.changeSortType);
+
 
 render(footerStatisticsElement, new FooterStatistics(films.length).getElement(), RenderPosition.BEFOREEND);
